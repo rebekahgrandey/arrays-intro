0128f38b893f30f76381b713338e7239918d383e
Rebekah Taylor <rebekahtdesigns@gmail.com>
2022-10-07 10:14:00 -0500

    completed files

---
+++ main.js
@@ -0,0 +1,16 @@
+const yellowFruit = "Banana"
+const orangeFruit = "Orange"
+const redFruit = "Apple"
+const greenFruit = "Watermelon"
+const blueFruit = "Blueberry"
+
+const fruits = []
+
+fruits.push(yellowFruit)
+fruits.push(orangeFruit)
+fruits.push(redFruit)
+fruits.push(greenFruit)
+fruits.push(blueFruit)
+
+console.log(fruits)
+console.log(yellowFruit)
